5ac74fd4a202aead2bad3ae511313d3a26b98a9b
Estella Li <liqx9979@gmail.com>
2022-11-07 19:44:48 -0800

    update recipeCard star img

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -128,26 +128,23 @@ class RecipeCard extends HTMLElement {
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
-    const template = `
-      <article>
-        <img src="https://link-to-article.com/recipe-thumbnail.jpg"
-          alt="Recipe Title">
-        <p class="title">
-          <a href="https://link-to-article.com">Title</a>
-        </p>
-        <p class="organization">The Chef's Organization</p>
-        <div class="rating">
-          <span>5</span>
-          <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-          <span>(500)</span>
-        </div>
-        <time>50 min</time>
-        <p class="ingredients">
-          Comma, Separated, List, of, Ingredients
-        </p>
-    </article>
-    `
-    article.body.innerHTML = template;
+    article.innerHTML = `
+    <img src= ${data["imgSrc"]}
+      alt= ${data["imgAlt"]}>
+    <p class="title">
+      <a href= ${data["titleLnk"]}>${data["titleTxt"]}</a>
+    </p>
+    <p class="organization">${data["organization"]}</p>
+    <div class="rating">
+      <span>${data["rating"]}</span>
+      <img src="./assets/images/icons/${data["rating"]}-star.svg" alt="${data["rating"]} stars" >
+      <span>${data["numRatings"]}</span>
+    </div>
+    <time>${data["lengthTime"]}</time>
+    <p class="ingredients">
+      ${data["ingredients"]}
+    </p>
+    `;
   }
 }
 
